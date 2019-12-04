477a6073345363b4555dbdb9b0fed46d3ba16224
7 <swistek.labs@gmail.com>
2019-12-04 22:30:02 +0000

    movement

--- play.js
+++ play.js
@@ -1,9 +1,30 @@
 const net = require("net");
 const { connect } = require("./client");
+const stdin = process.stdin;
+const stdout = process.stdout;
 /**
  * Establishes connection with the game server
  */
 
 console.log("Connecting ...");
 
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+
+stdin.on('data', (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+})
+
+setupInput();
 connect();
